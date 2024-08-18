a0c6f3f7c73940adafa5f845f7a4aabf9b9d08b7
GeimeAlondra <105683683+GeimeAlondra@users.noreply.github.com>
2024-08-18 13:25:57 -0600

    Guía XI - Organizando la capa de datos (finalizada)

---
+++ CapaConexion/DatosLayer/DataBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatosLayer
+{
+    public class DataBase
+    {
+        public static String ConnectionString
+        {
+            get
+            {
+                return ConfigurationManager
+                .ConnectionStrings["NWConnection"]
+                .ConnectionString;
+            }
+        }
+
+        //public static String ConnectionString1()
+        //{
+        //    return ConfigurationManager
+        //    .ConnectionStrings["NWConnection"]
+        //    .ConnectionString;
+        //}
+    }
+}
